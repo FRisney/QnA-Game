1b615a8562abc60066a3e97c53a42ff2f37ebbc6
FRisney <frisney@protonmail.com>
2020-06-09 14:56:08 -0300

    Adicao do script para gerenciar o painel de pergunta

---
+++ ui/painel_perguntas.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+onready var label_pergunta: = find_node("LaP")
+onready var label_alternativa_a: = find_node("LaAltA")
+onready var label_alternativa_b: = find_node("LaAltB")
+onready var anim_panel: = find_node("_anim")
+var text: Dictionary
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Events.connect("changed_destination", self, "_on_changed_destination")
+	Events.connect("finished_questions", self,"_on_finished_questions")
+	anim_panel.connect("animation_finished", self, "_on_finished_animation")
+
+
+func _on_changed_destination(data: Dictionary):
+	text = data
+	anim_panel.play("out")
+
+
+func _on_finished_animation(animation_name: String):
+	if animation_name == "out":
+		label_pergunta.text = text.per
+		if text.has("alt_a") and text.has("alt_b"):
+			label_alternativa_a.text = text.alt_a
+			label_alternativa_b.text = text.alt_b
+		anim_panel.play("in")
+
+
+func _on_finished_questions(data: Dictionary):
+	pass
