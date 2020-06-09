0f58f1343a7ecdfefcd158e2fe62a8356d5e1803
FRisney <frisney@protonmail.com>
2020-06-09 14:50:11 -0300

    Tratamento de entrada

--- main/signal_sinleton.gd
+++ main/signal_sinleton.gd
@@ -3,14 +3,12 @@ extends Node
 # Sinais que poderao ser usados a partir de qualquer Node a qualquer momento
 signal changed_destination(data)
 signal finished_questions(data)
-signal changed_choice(choice)
 signal started_tension
 
 
 func _ready() -> void:
 	Events.connect("started_tension", self, "_on_started_tension")
 	Events.connect("changed_destination", self, "_on_changed_destination")
-	Events.connect("changed_choice", self, "_on_changed_choice")
 
 
 func _on_started_tension():
@@ -21,5 +19,6 @@ func _on_changed_destination():
 	Audio.get_node("Resposta").play()
 
 
-func _on_changed_choice():
-	Audio.get_node("Alternativa").play()
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("alt_a") or event.is_action_pressed("alt_b"):
+		Audio.get_node("Alternativa").play()
