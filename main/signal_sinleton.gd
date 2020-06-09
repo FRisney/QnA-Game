f2b6b87ae59553a34c395ba554c31165dba5b355
FRisney <frisney@protonmail.com>
2020-06-09 12:29:28 -0300

    Gerenciamento de audio

--- main/signal_sinleton.gd
+++ main/signal_sinleton.gd
@@ -6,3 +6,20 @@ signal changed_destination(data)
 signal finished_questions(data)
 signal changed_choice(choice)
 signal started_tension
+
+func _ready() -> void:
+	Events.connect("started_tension", self, "_on_started_tension")
+	Events.connect("changed_destination", self, "_on_changed_destination")
+	Events.connect("changed_choice", self, "_on_changed_choice")
+
+
+func _on_started_tension():
+	Audio.get_node("Tensao").play()
+
+
+func _on_changed_destination():
+	Audio.get_node("Resposta").play()
+
+
+func _on_changed_choice():
+	Audio.get_node("Alternativa").play()
