0bc215f5370490928dfd8fa24029ec73edc040a2
FRisney <frisney@protonmail.com>
2020-06-09 11:24:49 -0300

    Criacao do modelo de pergunta

---
+++ point/pergunta.gd
@@ -0,0 +1,13 @@
+extends "res://point/Point.gd"
+
+export(Array, NodePath) var path_alternativas: Array
+export(String, MULTILINE) var pergunta := "Pergunta"
+export(String, MULTILINE) var alternativaA := "Alternativa A"
+export(String, MULTILINE) var alternativaB := "Alternativa B"
+
+func _ready() -> void:
+	# Declarar o tipo para pergunta
+	type = 0
+	# Guardar o caminho para as alternativas
+	path_alternativas.append(find_node("A").get_path())
+	path_alternativas.append(find_node("B").get_path())
