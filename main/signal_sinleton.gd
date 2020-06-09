76d7ca5253b7802fcec5f7caeaa659a04fb36dcc
FRisney <frisney@protonmail.com>
2020-06-09 12:03:23 -0300

    Signal Singleton

---
+++ main/signal_sinleton.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Sinais que poderao ser usados a partir de qualquer Node a qualquer momento
+
+signal changed_destination(data)
+signal finished_questions(data)
+signal changed_choice(choice)
+signal started_tension
